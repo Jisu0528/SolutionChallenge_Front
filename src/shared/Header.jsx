c49d59e54d741ca5aed6e3da04644c0ceded103a
Jisu0528 <0528jisu@gmail.com>
2024-02-01 20:55:26 +0900

    design: header ui 구현

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,7 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
 const Header = () => {
   return (
-    <div>header</div>
+    <Wrapper>
+      <Logo>SolutionChallenge</Logo>
+
+      <NavGroup>
+        <NavLinkStyled to='/'>Home</NavLinkStyled>
+        <NavLinkStyled to='/drugs'>Drugs</NavLinkStyled>
+        <NavLinkStyled to='/deathrate'>Death Rate</NavLinkStyled>
+        <NavLinkStyled to='/transformimage'>AI Image</NavLinkStyled>
+      </NavGroup>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.header`
+  display: flex;
+  margin: 40px auto;
+  padding: 0 120px;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+
+const Logo = styled.div`
+  font-size: 32px;
+  font-weight: 600;
+`;
+
+const NavGroup = styled.nav`
+  display: flex;
+  gap: 12px;
+`;
+
+const NavLinkStyled = styled(NavLink)`
+  text-decoration: none;
+  color: #3D3D3D;
+  font-size: 18px;
+  font-weight: 500;
+
+  &:hover {
+    color: #4B4EFC;
+  }
+
+  &.active {
+    color: #4B4EFC;
+    text-decoration: underline;
+  }
+`;
 
 export default Header;

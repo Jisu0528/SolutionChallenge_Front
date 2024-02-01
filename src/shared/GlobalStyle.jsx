d7ef93919188d041893bc5ac1c5973018c911185
Jisu0528 <0528jisu@gmail.com>
2024-02-01 19:55:01 +0900

    fix: color "" 삭제

--- src/shared/GlobalStyle.jsx
+++ src/shared/GlobalStyle.jsx
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
-    color: "#3D3D3D";
+    color: #3D3D3D;
     font-family: Arial, Helvetica, sans-serif;
     margin: 0 auto;
     padding: 0;

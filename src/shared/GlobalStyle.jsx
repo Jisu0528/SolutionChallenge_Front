ce9c34ab66c8868bf6c56ff93b51478dd120aa74
Jisu0528 <0528jisu@gmail.com>
2024-02-01 20:53:35 +0900

    feat: google font 적용

--- src/shared/GlobalStyle.jsx
+++ src/shared/GlobalStyle.jsx
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
   body {
     color: #3D3D3D;
-    font-family: Arial, Helvetica, sans-serif;
+    font-family: 'Inter', sans-serif;
     margin: 0 auto;
     padding: 0;
   }

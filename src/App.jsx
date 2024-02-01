779fcc804a187a4e87d76a76b6b7096c6b9471de
Jisu0528 <0528jisu@gmail.com>
2024-02-01 19:23:58 +0900

    feat: GlobalStyle 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Router from "./shared/Router";
 import Header from "./shared/Header";
 import Footer from "./shared/Footer";
+import GlobalStyle from "./shared/GlobalStyle";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Router />
       <Footer />

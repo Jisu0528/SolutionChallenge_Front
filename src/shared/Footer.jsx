79e3f4d5f58d7b70e1bcdbdfbe3c852052183fc6
Jisu0528 <0528jisu@gmail.com>
2024-02-18 05:18:55 +0900

    feat: Footer 구현 #1

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -1,7 +1,102 @@
+import styled from "styled-components";
+import Logo from '../assests/logo2.png';
+
 const Footer = () => {
   return (
-    <div>footer</div>
+    <Wrapper>
+      <Container>
+        <Img src={Logo} />
+        <People>
+          <Name>Jisu Han</Name>
+          <Role>
+            Frontend <br />
+            Design <br />
+            Backend <br />
+          </Role>
+          <Email href="010528jisu@gmail.com">0528jisu@gmail.com</Email>
+        </People>
+        <People>
+          <Name>Jisu Han</Name>
+          <Role>
+            Frontend <br />
+            Design <br />
+            Backend <br />
+          </Role>
+          <Email>0528jisu@gmail.com</Email>
+        </People>
+        <People>
+          <Name>Jisu Han</Name>
+          <Role>
+            Frontend <br />
+            Design <br />
+            Backend <br />
+          </Role>
+          <Email>0528jisu@gmail.com</Email>
+        </People>
+        <People>
+          <Name>Jisu Han</Name>
+          <Role>
+            Frontend <br />
+            Design <br />
+            Backend <br />
+          </Role>
+          <Email>0528jisu@gmail.com</Email>
+        </People>
+      </Container>
+      <CopyRight> ⓒ Copyright All rights reserved DrugGuadian</CopyRight>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  height: 300px;
+  background-color: #3D3D3D;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  width: 1530px;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 25px;
+`;
+
+const Img = styled.img`
+  width: 208px;
+  height: 77px;
+  padding-right: 100px;
+`;
+
+const People = styled.div`
+  color: #F6F6F6;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+`;
+
+const Name = styled.div`
+  font-size: 18px;
+`;
+
+const Role = styled.div`
+  font-size: 14px;
+  
+  text-align: center;
+`;
+
+const Email = styled.a`
+  font-size: 14px;
+  text-decoration: none;
+  color: inherit;
+`;
+
+const CopyRight = styled.div`
+  color: #A8A8A8;
+  font-size: 14px;
+`;
 
 export default Footer;
